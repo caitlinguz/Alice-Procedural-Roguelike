45db9d325a1fe341dfa12c0cefa4741601a2f4e9
Caitlin Guzman <cg1656@nyu.edu>
2015-03-26 14:15:21 -0400

    Path code done

---
+++ Assets/Scripts/GridInstantiate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridInstantiate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for ( int counter = 0 ; counter < 5 ; counter++ ) {
+			for ( int otherCounter = 0 ; otherCounter < 5 ; otherCounter++ ) {
+				transform.position += transform.forward * 5f;
+
+				float randomNumber = Random.Range (0f, 1f);
+
+
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+/*
+// put this on a small cube, it will instantiate a grid of 5x5 floors with random walls
+START:
+for ( integer X starts at zero; as long as X is less than 5; increment X):
+	for ( integer Z starts at zero; as long as Z is less than 5; increment Z):
+		Generate a temporary Vector3 "pos" of "(X, 0, Z) + current position"
+		70% chance to instantiate a floor tile prefab at "pos"
+		25% chance to instantiate a wall tile prefab at "pos"
+		5% chance to instantiate nothing
+Destroy self
+// when you are done, make sure you test this and it's working
+*/
---
+++ Assets/Scripts/PathInstantiate.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathInstantiate : MonoBehaviour {
+
+	int tilesPlacedCounter = 0;
+	public Transform floorTilePrefab; // assign in inspector
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tilesPlacedCounter < 50) {
+
+			float randomNum = Random.Range (0f, 1f);
+
+			if (randomNum < .25f) {
+				transform.Rotate (0f, 90f, 0f);
+			}
+			else if (randomNum < .5f) {
+				transform.Rotate (0f, -90f, 0f);
+			}
+
+			Instantiate ( floorTilePrefab, 
+			             transform.position,
+			             Quaternion.Euler ( 0f, 0f, 0f )
+			             );
+
+			transform.position += transform.forward * 5f;
+
+			tilesPlacedCounter++;
+		}
+
+		else {
+			Destroy (gameObject);
+		}
+
+	}
+}
+
+/*
+// put this on a small cube, it will move around and drop floor modules like breadcrumbs
+UPDATE:
+If counter is less than 50, then:
+	Generate random number from 0.0f to 1.0f
+	If number is less than 0.25f, then rotate 90 degrees
+	... Else if number is 0.25f-0.5f, then rotate -90 degrees
+	Instantiate a floor tile prefab at current position
+	Move forward (with respect to rotation) by 5 units
+	Increment counter
+Else
+	Destroy self
+// when you are done, make sure you test this and it's working
+*/
